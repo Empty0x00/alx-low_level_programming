8952f7e61ecc524c462e1c0922578ed77e22b602
Empty0x00 <mostafaaantri88@gmail.com>
2025-07-02 12:57:46 +0000

    added the description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: prints a string.
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
