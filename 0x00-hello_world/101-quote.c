70dcce4069471c513db276bad3fe921065b1a1e4
Empty0x00 <mostafaaantri88@gmail.com>
2025-07-02 14:29:57 +0000

    love loops

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print a string;
+ * @ft_putchar: print chars.
+ * @c: store chars.
+ *
+ * Return: 1.
+ */
+void ft_putchar(char *c)
+{
+	while (*c)
+	{
+		write(1, c, 1);
+		c++;
+	}
+}
+
+int main(void)
+{
+	ft_putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
